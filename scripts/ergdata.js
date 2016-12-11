ba0353e152d8fb083779620fd857b989c400d133
clivekeeler <clive.keeler@gmail.com>
2016-12-11 18:21:23 +0000

    Update ergdata.js

--- scripts/ergdata.js
+++ scripts/ergdata.js
@@ -1,29 +1,42 @@
 function myFunction() {
-  var d, min, sec, w, ds, t, sc, scm, scs, p, P, f, at, atm, ats, ad, sp;
+  var dis, min, sec, w, ds, t, sc, scm, scs, P, f, at, atm, ats, wt, wm, ws, ad, sp, spm, sps, pt, pm, ps;
 
   // Get the value of the input field by id
-  d = document.getElementById("distance").value;
+  dis = document.getElementById("distance").value;
   min = document.getElementById("min").value;
   sec = document.getElementById("sec").value;
   w = document.getElementById("weight").value;
   ds = document.getElementById("setDist").value;
 
   t = (min * 60) + (sec / 1);
-  sc  = t / d * 500;
+
+  sc  = t / dis * 500;
   scm = Math.floor(sc / 60);
   scs = sc % 60;
-  p  = t / d;
-  P  = 2.8 / Math.pow(t/d, 3);
+
+  P  = 2.8 / Math.pow(t / dis, 3);
+
   f  = Math.pow(w / 122.47, .222);
-  at = f * t;
+
+  at = f * sc;
   atm = Math.floor(at / 60);
   ats = at % 60;
-  ad = d / f;
-  sp = 5*((Math.log(ds/d))/(Math.log(2))) + sc;
+
+  wt = at * dis / 500;
+  wm = Math.floor(wt / 60);
+  ws = wt % 60;
+
+  ad = dis / f;
+
+  sp = 5*((Math.log(ds / dis)) / (Math.log(2))) + sc;
   spm = Math.floor(sp / 60);
   sps = sp % 60;
 
-  document.getElementById("dist").innerHTML = "Distance : " + d;
+  pt = sp * ds / 500;
+  pm = Math.floor(pt / 60);
+  ps = pt % 60;
+
+  document.getElementById("dist").innerHTML = "Distance : " + dis;
   if (sec < 10) {
     document.getElementById("time").innerHTML = "Time : " + min + ":0" + sec;
   }
@@ -31,32 +44,43 @@ function myFunction() {
     document.getElementById("time").innerHTML = "Time : " + min + ":" + sec;
   }
   if (scs < 10) {
-    document.getElementById("split").innerHTML = "Split time : " + scm + ":0" + scs.toFixed(1);
+    document.getElementById("split").innerHTML = "Split : " + scm + ":0" + scs.toFixed(1);
   }
   else {
-    document.getElementById("split").innerHTML = "Split time : " + scm + ":" + scs.toFixed(1);
+    document.getElementById("split").innerHTML = "Split : " + scm + ":" + scs.toFixed(1);
   }
-  //document.getElementById("pace").value = p;
+
   document.getElementById("power").innerHTML = "Power      : " + P.toFixed();
-  //if weight blank ...
-  //document.getElementById("wf").innerHTML = "Weight factor : " + f.toFixed(3);
-  //document.getElementById("mass").innerHTML = "Weight : " + w;
+
   if (ats < 10) {
-    document.getElementById("adTime").innerHTML = "Adj. Split : " + atm + ":0" + ats.toFixed(1);
+    document.getElementById("adTime").innerHTML = "Adj. time : " + wm + ":0" + ws.toFixed(1);
   }
   else {
-    document.getElementById("adTime").innerHTML = "Adj. Split : " + atm + ":" + ats.toFixed(1);
+    document.getElementById("adTime").innerHTML = "Adj. time : " + wm + ":" + ws.toFixed(1);
   }
-  //document.getElementById("adTimeSec").innerHTML = ats.toFixed(1);
-  document.getElementById("adDist").innerHTML = "Adj. distance : " + ad.toFixed();
-  //if set dist blank ...
+
+  if (ats < 10) {
+    document.getElementById("adSplit").innerHTML = "Adj. split : " + atm + ":0" + ats.toFixed(1);
+  }
+  else {
+    document.getElementById("adSplit").innerHTML = "Adj. split : " + atm + ":" + ats.toFixed(1);
+  }
+
+  document.getElementById("preDist").innerHTML = "Distance : " + ds;
+
+  if (ps < 10) {
+    document.getElementById("preTime").innerHTML = "Predicted time : " + pm + ":0" + ps.toFixed(1);
+  }
+  else {
+    document.getElementById("preTime").innerHTML = "Predicted time : " + pm + ":" + ps.toFixed(1);
+  }
+
   if (sps < 10) {
-    document.getElementById("preSplit").innerHTML = "For " + ds + " m, predicted split : " + spm + ":0" + sps.toFixed(1);
+    document.getElementById("preSplit").innerHTML = "Predicted split : " + spm + ":0" + sps.toFixed(1);
   }
   else {
-    document.getElementById("preSplit").innerHTML = "For " + ds + " m, predicted split : " + spm + ":" + sps.toFixed(1);
+    document.getElementById("preSplit").innerHTML = "Predicted split : " + spm + ":" + sps.toFixed(1);
   }
-  //document.getElementById("preSplitSec").innerHTML = sps.toFixed(1);
 
   if (document.getElementById("inputSection").style.display == "none") {
     document.getElementById("inputSection").style.display = "block";
@@ -68,6 +92,4 @@ function myFunction() {
     document.getElementById("resultsSection").style.display = "block";
     document.getElementById("button").innerHTML = "Reset";
   }
-
-
 }
